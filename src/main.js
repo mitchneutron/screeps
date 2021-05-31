3aa1f2c62886bae970f7c222fa08cdfc07941d73
Mitchell Mortensen <mitchell.mortensen@fishbowlinventory.com>
2021-05-31 11:08:17 -0600

    Initial commit.

---
+++ src/main.js
@@ -0,0 +1,34 @@
+var roleHarvester = require('src/role.harvester');
+var roleUpgrader = require('src/role.upgrader');
+var roleBuilder = require('src/role.builder');
+
+module.exports.loop = function () {
+
+    var tower = Game.getObjectById('25f67b52d0e1dd3923431326');
+    if(tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+}
