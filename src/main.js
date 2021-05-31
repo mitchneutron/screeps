29a058cd89043d376bff890c943cbac1c7936cb7
Mitchell Mortensen <mitchell.mortensen@fishbowlinventory.com>
2021-05-31 11:57:58 -0600

    More changes to development environment. Should be most the way there.

--- src/main.js
+++ src/main.js
@@ -21,13 +21,13 @@ module.exports.loop = function() {
 
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
-        if (creep.memory.role == 'harvester') {
+        if (creep.memory.role === 'harvester') {
             roleHarvester.run(creep);
         }
-        if (creep.memory.role == 'upgrader') {
+        if (creep.memory.role === 'upgrader') {
             roleUpgrader.run(creep);
         }
-        if (creep.memory.role == 'builder') {
+        if (creep.memory.role === 'builder') {
             roleBuilder.run(creep);
         }
     }
